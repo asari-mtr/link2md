028c6427b1c5b8450d1656124267a296798b4f79
Asari Mitsuteru <mitsuteru.asari@supership.jp>
2018-05-25 12:14:51 +0900

    add popup

--- background.js
+++ background.js
@@ -1,7 +1,13 @@
-'use strict'
+'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('Hello');
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        // TODO: allow all host
+        pageUrl: {hostEquals: 'developer.chrome.com'},
+      })],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
   });
 });
