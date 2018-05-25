e33a405b874eeba195aa9add359bd1a5402a99be
Asari Mitsuteru <mitsuteru.asari@supership.jp>
2018-05-25 18:04:11 +0900

    implement for get all link

--- popup.js
+++ popup.js
@@ -1,5 +1,29 @@
 let copyAsMd = document.getElementById('copyAsMd');
 
 copyAsMd.onclick = function(e) {
-  chrome.extension.getBackgroundPage().console.log("click");
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+      tabs[0].id,
+      {code:
+        `
+const selection = window.getSelection()
+
+const dom = selection.getRangeAt(0).cloneContents();
+
+const links = dom.querySelectorAll('a');
+
+var linkTexts = [];
+for (const link of links) {
+  const href = link.getAttribute('href');
+  linkTexts.push(href);
+}
+linkTexts;
+        `
+        },
+      function(results) {
+        // TODO: I can only get it for the first time
+        chrome.extension.getBackgroundPage().console.log(results);
+      }
+    );
+  });
 };
