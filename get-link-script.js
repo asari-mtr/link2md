2471ef740ea79cf2453b3c1ad79f6dce8f69fae8
Asari Mitsuteru <asari.mtr@gmail.com>
2018-05-27 11:45:15 +0900

    fix relative url to absolute url

--- get-link-script.js
+++ get-link-script.js
@@ -1,10 +1,14 @@
-var selection = window.getSelection()
+var selection = window.getSelection();
 var dom = selection.getRangeAt(0).cloneContents();
 var links = dom.querySelectorAll('a');
+var origin = location.origin;
 
 var linkTexts = [];
 for (const link of links) {
-  const href = link.getAttribute('href');
+  var href = link.getAttribute('href');
+  if (href.startsWith("/")) {
+    href = origin.concat(href);
+  }
   const text = link.text.trim();
   const list = "- [ ] [" + text + "](" + href + ")";
   linkTexts.push(list);
