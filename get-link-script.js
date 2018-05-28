3ddf6e8e5ed506e8f891aa019bc75ce162b0d177
Asari Mitsuteru <asari.mtr@gmail.com>
2018-05-28 23:05:50 +0900

    fix internal links

--- get-link-script.js
+++ get-link-script.js
@@ -6,7 +6,7 @@ var origin = location.origin;
 var linkTexts = [];
 for (const link of links) {
   var href = link.getAttribute('href');
-  if (href.startsWith("/")) {
+  if (href.startsWith("/") || href.startsWith("#")) {
     href = origin.concat(href);
   }
   const text = link.text.trim();
